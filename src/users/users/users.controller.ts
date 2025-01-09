3613856b025f3e6433b4e89900b8c65fbcf5a8c1
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-09 07:50:31 +0000

    Create UsersController for handling user profile retrieval and updates

---
+++ src/users/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Body, Req, UseGuards, Patch } from '@nestjs/common';
+import { UsersService } from '../users.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard/jwt-auth.guard';
+import { User } from 'src/model/user.entity';
+import { UpdateProfileDto } from 'src/auth/dto/update-profile.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Req() req): Promise<{ data: { user: User } }> {
+    const user = req.user;
+    return { data: { user } };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('profile')
+  async updateProfile(
+    @Req() req,
+    @Body() updateData: UpdateProfileDto,
+  ): Promise<{ data: { user: User } }> {
+    const updatedUser = await this.usersService.update(req.user.id, updateData);
+    return { data: { user: updatedUser } };
+  }
+}
