b84b64c55d20fd0d4ec4d6a6dc17c91d7487a695
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-09 07:39:51 +0000

    Implement JwtStrategy for JWT authentication using Passport

---
+++ src/auth/jwt.strategy/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/articles/interface/types';
+import { User } from 'src/model/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || '',
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const user = await this.usersService.findById(payload.user.id);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
