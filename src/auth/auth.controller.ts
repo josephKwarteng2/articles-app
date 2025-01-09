119e869a1fe4674cd9e1f55b3a11cf8d047dc5a6
josephKwarteng2 <josephkwarteng244@gmail.com>
2025-01-09 15:51:01 +0000

    Refactor AuthController to use global exception filter

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
 import { User } from 'src/model/user.entity';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
-@Controller('/api/auth')
+@Controller('/api/v1/auth')
+@UseFilters(HttpExceptionFilter)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
@@ -21,7 +22,9 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() reqBody: LoginDto) {
+  async login(
+    @Body() reqBody: LoginDto,
+  ): Promise<{ data: { access_token: string; user: Partial<User> } }> {
     const user = await this.authService.validateUser(
       reqBody.email,
       reqBody.password,
